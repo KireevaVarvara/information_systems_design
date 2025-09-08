862b8597fc46a3536deccc4f676da76a659f72ed
KireevaVarvara <144010162+KireevaVarvara@users.noreply.github.com>
2025-09-08 22:05:22 +0300

    LR1.4 (made statistical methods of validation class)

--- travel_agency/Client.py
+++ travel_agency/Client.py
@@ -1,3 +1,5 @@
+import re
+
 class Client:
     def __init__(self, firstname, surname, fathers_name, phone_number, pasport, email, balance=None):
         self.set_firstname(firstname)
@@ -9,25 +11,25 @@ class Client:
         self.set_balance(balance)
 
     def set_firstname(self, firstname):
-        self._firstname = firstname
+        self._firstname = self.__validate_fio(firstname)
     def set_surname(self, surname):
-        self._surname = surname
+        self._surname = self.__validate_fio(surname)
     def set_fathers_name(self, fathers_name):
-        self._fathers_name = fathers_name
+        self._fathers_name = self.__validate_fio(fathers_name, True)
     def set_phone_number(self, phone_number):
-        self._phone_number = phone_number
+        self._phone_number = self.__validate_phone_number(phone_number)
     def set_pasport(self, pasport):
-        self._pasport = pasport
+        self._pasport = self.__validate_pasport(pasport)
     def set_email(self, email):
-        self._email = email
+        self._email = self.__validate_email(email)
     def set_balance(self, balance):
-        self._balance = balance
+        self._balance = self.__validate_balance(balance)
 
     def get_firstname(self):
         return self._firstname
     def get_surname(self):
         return self._surname
-    def get_fathersname(self):
+    def get_fathers_name(self):
         return self._fathers_name
     def get_phone_number(self):
         return self._phone_number
@@ -37,6 +39,43 @@ class Client:
         return self._email
     def get_balance(self):
         return self._balance
+
+    @staticmethod
+    def __validate_fio(fio_field, is_fathers_name=False):
+        if not isinstance(fio_field, str) and not is_fathers_name:
+            raise ValueError('Имя и фамилия должны быть строковыми')
+        if not fio_field and not is_fathers_name:
+            raise ValueError('ФИО не должно быть пустым')
+        if ((is_fathers_name and fio_field is not None) or not is_fathers_name) and re.search(r'\d', fio_field) is not None:
+            raise ValueError('ФИО не должно содержать цифр')
+        return fio_field
+
+    @staticmethod
+    def __validate_phone_number(phone_number):
+        if not isinstance(phone_number, str) or not re.fullmatch(r'((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}', phone_number):
+            raise ValueError('Неверный номер телефона')
+        return phone_number
+
+    @staticmethod
+    def __validate_pasport(pasport):
+        if not isinstance(pasport, str) or not re.fullmatch(r'\d{4} \d{6}', pasport):
+            raise ValueError('Неверные паспортные данные')
+        return pasport
+
+    @staticmethod
+    def __validate_email(email):
+        if not isinstance(email, str) or not re.fullmatch(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)', email):
+            raise ValueError('Неверный email')
+        return email
+
+    @staticmethod
+    def __validate_balance(balance):
+        if balance is not None:
+            if not isinstance(balance, (float, int)):
+                raise ValueError('Баланс должен быть числом')
+            if balance < 0:
+                raise ValueError('Баланс не должен быть отрицательным')
+        return balance
 
     def __str__(self):
         return f'{self._surname} {self._firstname}'
